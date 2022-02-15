a3829089f98b4c669fd6b91ecdf9000e437c9227
Jalome <chjalome@gmail.com>
2022-02-15 16:13:22 +0200

    updated port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use('/static',express.static(static_path))
 
 // initialise ports and hostname
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //server using http module
 // const server = http.createServer((req, res) => {
